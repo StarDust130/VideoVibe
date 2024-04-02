7b7a781a9e69f041619cc9adabc1fd2431e84cbd
ChandraShekhar <csyadav0513@gmail.com>
2024-04-02 14:01:55 +0530

    Update greeting text to capitalize username

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -28,8 +28,8 @@ const Home = () => {
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className=" max-w-[273px] rounded py-2 text-start text-base font-sans font-bold lg:text-2xl">
-            {user?.username ? `Hello, ${user.username}` : "Hello"}
+          <h2 className=" max-w-[273px] capitalize rounded py-2 text-start text-base font-sans font-bold lg:text-2xl">
+            {user?.username ? `Hello, ${user.username} ` : "Hello"}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
