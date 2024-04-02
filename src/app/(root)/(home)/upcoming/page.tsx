1bf3f9e1418cee4fa37edacf1bff9bf16a26ac65
ChandraShekhar <csyadav0513@gmail.com>
2024-04-02 08:14:04 +0530

    Add CallList component to Previous, Recordings, and Upcoming pages

--- src/app/(root)/(home)/upcoming/page.tsx
+++ src/app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,11 @@
+import CallList from "@/components/CallList";
 
 const Upcoming = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Upcomings</h1>
+      <CallList type="upcoming" />
     </section>
   );
-}
-export default Upcoming
+};
+export default Upcoming;
