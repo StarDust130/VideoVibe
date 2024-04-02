0da83a3968af6b414c797d68d03a1cab84cfc87d
ChandraShekhar <csyadav0513@gmail.com>
2024-04-02 11:14:49 +0530

    Add user greeting to home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState, useEffect } from "react";
 import MeetingTypeList from "@/components/MeetingTypeList";
+import { useUser } from "@clerk/nextjs";
 
 const Home = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const { user } = useUser();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,8 +28,8 @@ const Home = () => {
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+          <h2 className=" max-w-[273px] rounded py-2 text-start text-base font-sans font-bold lg:text-2xl">
+            {user?.username ? `Hello, ${user.username}` : "Hello"}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
